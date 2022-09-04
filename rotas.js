e687d859d72e68074ffaa7add5926c6c74e6dd3c
thschmitz <thomas.henrique.schmitz@gmail.com>
2022-09-04 20:31:16 -0300

    Testing

--- rotas.js
+++ rotas.js
@@ -2,7 +2,6 @@ const posts = require('./src/posts');
 const usuarios = require('./src/usuarios');
 
 module.exports = app => {
-  app.get('/', (req, res) => {res.send('Olá pessoa!')});
   
   posts.rotas(app);
   usuarios.rotas(app);
--- server.js
+++ server.js
@@ -5,6 +5,8 @@ require("dotenv").config();
 require("./redis/blocklist-access-token")
 require("./redis/allowlist-refresh-token")
 
+app.get('/', (req, res) => {res.send('Olá pessoa!')});
+
 app.use((req,res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
